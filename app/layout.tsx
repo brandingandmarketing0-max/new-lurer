66b973e59c48685ad65695d53334a0b636752b14
brandingandmarketing0-max <brandingandmarketing0@gmail.com>
2025-09-18 03:06:54 +0530

    AS

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Lure - Fidelity',
+  title: 'Tapit - Fidelity',
   description: 'Fidelity MGMT',
 }
 
